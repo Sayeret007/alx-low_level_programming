b203524b1b788d07e1372ec4cbf08644c0c47a2f
Sayeret007 <esezoborimonitie@gmail.com>
2022-04-07 22:23:23 -0700

    first c script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{printf("/Programming is like building a multilingual puzzle");
-puts (0);
+{puts("/Programming is like building a multilingual puzzle");
+return (0);
 }
