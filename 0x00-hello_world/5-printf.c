ef2986b6920daaaa03013ac7bfe337683c25dfb7
Sayeret007 <esezoborimonitie@gmail.com>
2022-04-07 22:36:26 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,6 @@
  * Return: 0 if succesful
  */
 int main(void)
-{puts("with proper grammar, but the outcome is a piece of art,\n");
+{printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
