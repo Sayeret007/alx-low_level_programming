8036eb882b19245c5865c56339689265404562a6
Sayeret007 <esezoborimonitie@gmail.com>
2022-04-07 22:16:15 -0700

    first c script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
  *
- *  * main - The containing function of this programme
- *
- *   * Return: this gives 0 if sucessful
- *
- *    */
+ *  main - The containing function of this programme
+ *  Return: this gives 0 if sucessful
+ */
 int main(void)
-{		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+{ puts("/Programming is like building a multilingual puzzle");
+return (0);
